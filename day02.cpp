7b84a0831ddcc19e24b40bcc63cb0ded29fb99e5
TheYuenMG <TheYuenMG@protonmail.com>
2025-12-13 23:32:47 -0800

    Add safety check for 2-1, solve 2-2

--- day02.cpp
+++ day02.cpp
@@ -59,6 +59,11 @@ long long part1(const vector<string>& lines)
         for (long long i = lower; i <= upper; i++)
         {
             int digits = std::to_string(i).size();
+            if (digits > POW10_MAXIMUM)
+            {
+                std::cout << "ID too large: " << digits << " digits, maximum 20" << std::endl;
+                continue;
+            }
             if (!(digits % 2))
             {
                 long long factor = pow10[digits / 2] + 1;
@@ -79,7 +84,49 @@ long long part1(const vector<string>& lines)
 //     returns the computed Part 2 result as a 64-bit integer
 long long part2(const vector<string>& lines)
 {
-    return 0;
+    long long result = 0;
+    string input = lines[0];
+    for (int i = 0; i < input.length(); i++)
+    {
+        if (input[i] == COMMA || input[i] == HYPHEN_MINUS)
+        {
+            input[i] = WHITESPACE;
+        }
+    }
+    long long lower, upper;
+    std::stringstream ranges(input);
+    vector<long long> pow10(POW10_MAXIMUM, 1);
+    for (int i = 1; i < POW10_MAXIMUM; i++)
+    {
+        pow10[i] = pow10[i-1] * 10;
+    }
+    while (ranges >> lower >> upper)
+    {
+        for (long long i = lower; i <= upper; i++)
+        {
+            int digits = std::to_string(i).size();
+            if (digits > POW10_MAXIMUM)
+            {
+                std::cout << "ID too large: " << digits << " digits, maximum 20" << std::endl;
+                continue;
+            }
+            for (int j = 1; j <= digits / 2; j++)
+            {
+                if (!(digits % j) && digits / j >= 2)
+                {
+                    long long numerator = pow10[digits] - 1;
+                    long long denominator = pow10[j] - 1;
+                    long long factor = numerator / denominator;
+                    if (!(i % factor) && int(std::to_string(i / factor).size()) == j)
+                    {
+                        result += i;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return result;
 }
 
 // orchestrate input parsing and rendering of both part results
@@ -91,7 +138,7 @@ void solve(ostream& out)
 {
     const vector<string> lines = parse_input();
     out << "Day 02 - Part 1: " << part1(lines) << '\n'; // Answer: 16793817782
-    out << "Day 02 - Part 2: " << part2(lines) << '\n';
+    out << "Day 02 - Part 2: " << part2(lines) << '\n'; // Answer: 27469417404
 }
 
 // program entry point delegating to solve using standard output
